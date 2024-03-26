7f27239eea44b7ea0e59f0e35290d2b0692c8a36
Scott Crowder <scrowder@gmail.com>
2024-03-26 13:55:32 -0500

    Initial Commit

---
+++ DataStore/DataStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStoreApp.swift
+//  DataStore
+//
+//  Created by SCOTT CROWDER on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DataStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
